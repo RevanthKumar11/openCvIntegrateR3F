0b446d0b28a99f611da1e92bc2fe972a43a48087
Revanth Kumar R <111222012+RevanthKumar11@users.noreply.github.com>
2023-09-29 11:20:16 +0530

    Update and rename Ball.py to detectThecolor.py
    It detects the color and finds the midpoint of the object.

--- detectThecolor.py
+++ detectThecolor.py
@@ -6,11 +6,11 @@ import json
 import socket
 
 
-myColorFinder = ColorFinder(False)
+myColorFinder = ColorFinder(False) # When the 'ColorFinder' is set to 'true,' it displays a popup adjustment bar.
 hsvVals = {'hmin': 90, 'smin': 77, 'vmin': 101, 'hmax': 109, 'smax': 239, 'vmax': 244}
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverAddressPort = ('192.168.137.1', 1999) 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP 
+serverAddressPort = ('0.0.0.0', 1999) # your IP adders
 
 def video():
     camera = cv2.VideoCapture(0)
@@ -20,6 +20,7 @@ def video():
         success, org_img = camera.read()
         h, w, _ = org_img.shape
         imgColor, mask = myColorFinder.update(org_img, hsvVals)
+        #findContours -> its find the midpoint of the object
         imgContour, contours = cvzone.findContours(org_img, mask, minArea= 1000)
         # imgStack = cvzone.stackImages([org_img, imgColor, mask, imgContour], 2, 0.5)
 
@@ -28,12 +29,14 @@ def video():
                     h - contours[0]['center'][1],\
                     int(contours[0]['area'])
             print(data)
+            # send the data to the nodejs
             sock.sendto(str.encode(str(data)), serverAddressPort)
             
            
 
         # show the camera``
         # cv2.imshow('image', imgStack)
+        # resize of the image
         imgContour =  cv2.resize(imgContour, (0, 0), None, 0.3, 0.3)
         cv2.imshow('imgContour', imgContour)
         #
